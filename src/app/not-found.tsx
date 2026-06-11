7643a68530db21e2b0b05101547993a682bb71b2
jomsjovelo <jomsjovelo@users.noreply.github.com>
2026-06-11 11:32:31 +0800

    fix: add not-found.tsx to fix build error

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24 text-center">
+      <h2 className="text-3xl font-black mb-4">404 - Not Found</h2>
+      <p className="text-slate-500">Could not find requested resource</p>
+    </div>
+  );
+}
